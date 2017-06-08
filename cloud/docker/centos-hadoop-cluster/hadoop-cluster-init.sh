8de30e38ea326465e541020ac59100de746c8179
txazo <784990655@qq.com>
2017-06-08 23:46:46 +0800

    update

--- cloud/docker/centos-hadoop-cluster/hadoop-cluster-init.sh
+++ cloud/docker/centos-hadoop-cluster/hadoop-cluster-init.sh
@@ -7,7 +7,7 @@ source ./hadoop-cluster-env.sh
 hostname=`hostname`
 
 # 初始化master节点etc/hadoop/slaves
-if [ $hostname == 'master' ]; then
+if [ $hostname = 'master' ]; then
     cd /usr/local/hadoop
     echo ${nodeHost[*]} | awk -F " " '{for (i = 1; i <= NF; i++) {if ($i != "master") {print $i}}}' > etc/hadoop/slaves
 fi
@@ -16,9 +16,10 @@ fi
 for ((i=0; i < ${#nodeHost[*]}; i++))
 do
     if [ "${nodeHost[$i]}" != "${hostname}" ]; then
-        echo "${nodeIP[$i]} ${nodeHost[$i]}" >> /etc/hosts
+        echo -e "${nodeIP[$i]} \t${nodeHost[$i]}" >> /etc/hosts
     fi
 done
 
+# 初始化ssh
 ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa >/dev/null 2>&1
 echo `cat ~/.ssh/id_rsa.pub`
