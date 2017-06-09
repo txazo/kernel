e9c47c0cb73940794b86d28363993440df7d4c07
txazo <784990655@qq.com>
2017-06-09 11:53:35 +0800

    update

--- cloud/docker/centos-hadoop-cluster/hadoop-cluster-start.sh
+++ cloud/docker/centos-hadoop-cluster/hadoop-cluster-start.sh
@@ -8,3 +8,7 @@ bin/hdfs namenode -format hadoop-cluster
 sbin/start-dfs.sh
 sbin/start-yarn.sh
 sbin/mr-jobhistory-daemon.sh --config etc/hadoop start historyserver
+
+# NameNode:                     http://192.168.99.100:50070
+# ResourceManager:              http://192.168.99.100:8088
+# MapReduce JobHistory Server:  http://192.168.99.100:19888
