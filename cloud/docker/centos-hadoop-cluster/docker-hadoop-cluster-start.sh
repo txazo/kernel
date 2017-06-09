c53415eddfbce2fe549169d4c0bcedb007ea2efa
txazo <784990655@qq.com>
2017-06-09 10:11:24 +0800

    update

--- cloud/docker/centos-hadoop-cluster/docker-hadoop-cluster-start.sh
+++ cloud/docker/centos-hadoop-cluster/docker-hadoop-cluster-start.sh
@@ -4,6 +4,7 @@ source ./docker-util.sh
 source ./hadoop-cluster-env.sh
 
 # 初始化network
+echo "Init Network: ${networkName}"
 docker network ls | awk 'NR > 1 {
     if ($2 == "'${networkName}'") {
         exists = 1;
@@ -13,10 +14,8 @@ docker network ls | awk 'NR > 1 {
         print 1;
     }
 }' | while read line; do
-    docker network create --subnet=${networkSubnet} ${networkName}
+    docker network create --subnet=${networkSubnet} ${networkName} >/dev/null 2>&1
 done
-
-masterPort=`echo ${masterExposePort[*]} | awk -F " " '{for (i = 1; i <= NF; i++) print (i == 1 ? "-p " : " -p ")$i":"$i}'`
 
 cleanContainer
 
@@ -25,21 +24,27 @@ for ((i=0; i < ${#nodeHost[*]}; i++))
 do
     cmd="docker run --name ${nodeHost[$i]} -h ${nodeHost[$i]} --network ${networkName} --ip ${nodeIP[$i]} -d -P -p ${nodeSSHPort[$i]}:22"
     if [ "${nodeHost[$i]}" = "master" ]; then
-        eval $cmd $masterPort ${imageName}
+        masterPort=`echo ${masterExposePort[*]} | awk -F " " '{for (i = 1; i <= NF; i++) print (i == 1 ? "-p " : " -p ")$i":"$i}'`
+        eval $cmd $masterPort ${imageName} ">/dev/null 2>&1"
     else
-        eval $cmd ${imageName}
+        eval $cmd ${imageName} ">/dev/null 2>&1"
     fi
+    echo "Hadoop Cluster Node [${nodeHost[$i]}]: started"
 done
 
 rm -f authorized_keys
 for ((i=0; i < ${#nodeHost[*]}; i++))
 do
-    scp -P ${nodeSSHPort[$i]} hadoop-cluster-init.sh hadoop-cluster-env.sh root@${containerIP}:/root
+    echo "Hadoop Cluster Node [${nodeHost[$i]}]: init"
+    scp -P ${nodeSSHPort[$i]} hadoop-cluster-init.sh hadoop-cluster-env.sh root@${containerIP}:/root >/dev/null 2>&1
     echo `ssh root@${containerIP} -p ${nodeSSHPort[$i]} 'chmod u+x hadoop-cluster-init.sh; ./hadoop-cluster-init.sh'` >> authorized_keys
 done
 
 for ((i=0; i < ${#nodeHost[*]}; i++))
 do
-    scp -P ${nodeSSHPort[$i]} authorized_keys root@${containerIP}:/root
-    echo `ssh root@${containerIP} -p ${nodeSSHPort[$i]} 'cat authorized_keys >> /root/.ssh/authorized_keys'`
+    echo "Hadoop Cluster Node [${nodeHost[$i]}]: add authorized_keys"
+    scp -P ${nodeSSHPort[$i]} authorized_keys root@${containerIP}:/root >/dev/null 2>&1
+    ssh root@${containerIP} -p ${nodeSSHPort[$i]} 'cat authorized_keys >> /root/.ssh/authorized_keys' >/dev/null 2>&1
 done
+
+echo "Hadoop Cluster start successed"
--- cloud/docker/centos-hadoop-cluster/docker-util.sh
+++ cloud/docker/centos-hadoop-cluster/docker-util.sh
@@ -3,11 +3,12 @@
 # Usage: Docker Util
 
 function cleanContainer() {
-    docker ps -a | awk '{print $1}' | while read cid
+    docker ps -a | awk 'NR > 1 {print $1}' | while read cid
     do
-        if [ ${cid} != 'CONTAINER' ]; then
-           docker stop ${cid}
-           docker rm -f ${cid}
-        fi
+        docker stop ${cid} >/dev/null 2>&1
+        docker rm -f ${cid} >/dev/null 2>&1
+        echo "Clean Container: ${cid}"
     done
 }
+
+cleanContainer
